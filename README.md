00191d309875f145f34b61c37a8c6c868d3ff0f6
amv <basil96@users.noreply.github.com>
2022-02-04 10:46:37 -0500

    Configurations, Part 1
    * Add `setup.cfg`, drop `docs/requirements.txt`
    
    * Add `.readthedocs.yaml` config for RTD.
    
    * Update docs version, README.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Sandbox for learning ReadTheDocs structure.
 
-One major focus is on TeX support and creation of TikZ figures on the fly by Sphinx.
+Emphasis on TeX support and creation of TikZ figures on the fly by Sphinx.
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, AMV'
 author = 'AMV'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
