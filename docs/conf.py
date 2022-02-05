c507961cc9f57029053ab42a0bfbc710715b8472
amv <basil96@users.noreply.github.com>
2022-02-05 00:04:02 -0500

    Style external links

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'style_external_links': True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
